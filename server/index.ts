2edf4ef6742166345edbc8d6045578bd5b2354fa
Ahmed Elkattan <ahmedamrelkattan@gmail.com>
2021-12-23 12:43:50 +0200

    Update server with the 3 main scenarios

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,10 @@ import KoaPug from 'koa-pug';
 
 const app = (module.exports = new Koa());
 const router = new KoaRouter();
+// eslint-disable-next-line no-new
 new KoaPug({
   viewPath: 'server/views',
-  app: app,
+  app,
 });
 
 // Utility functions
@@ -15,20 +16,48 @@ const randomString = (length = 20) =>
     .toString()
     .substring(2, length + 2);
 
+// HTTP Cookie Routes
 // Initial welcome page route
-router.get('/', async function (ctx: Context) {
-  await ctx.render('welcome');
+router.get('/http-cookie', async function (ctx: Context) {
+  await ctx.render('http-cookie/welcome');
 });
 
 // Auth page where cookie is set
-router.post('/auth', async function (ctx: Context) {
+router.post('/http-cookie/auth', async function (ctx: Context) {
+  const cookie = randomString();
+  ctx.cookies.set('auth', cookie, {httpOnly: true});
+  await ctx.render('http-cookie/auth', {cookie: cookie});
+});
+
+// Script Cookie Routes
+// Initial welcome page route
+router.get('/script-cookie', async function (ctx: Context) {
+  await ctx.render('script-cookie/welcome');
+});
+
+// Auth page where cookie is set
+router.post('/script-cookie/auth', async function (ctx: Context) {
+  const cookie = randomString();
+  ctx.cookies.set('auth', cookie, {httpOnly: false});
+  await ctx.render('script-cookie/auth', {cookie: cookie});
+});
+
+// Token Communication Routes
+// Initial welcome page route
+router.get('/communication', async function (ctx: Context) {
+  await ctx.render('communicate-token/welcome');
+});
+
+// Auth page where cookie is set
+router.post('/communication/auth', async function (ctx: Context) {
   const cookie = randomString();
   ctx.cookies.set('auth', cookie);
-  await ctx.render('auth', {cookie: cookie});
+  await ctx.render('communicate-token/auth', {cookie: cookie});
 });
 
 // Using defined routes
 app.use(router.routes()).use(router.allowedMethods());
 
-if (require.main)
+if (require.main) {
   app.listen(3000, () => console.log('listening on http://localhost:3000'));
+}

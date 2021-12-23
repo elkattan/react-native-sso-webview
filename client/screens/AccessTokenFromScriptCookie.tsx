afd202906f32336db899593712b3731caa76cc5d
Ahmed Elkattan <ahmedamrelkattan@gmail.com>
2021-12-23 12:46:07 +0200

    Get Token from non-HttpOnly token

--- client/screens/AccessTokenFromScriptCookie.tsx
+++ client/screens/AccessTokenFromScriptCookie.tsx
@@ -1,19 +1,51 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import CookieManager from '@react-native-cookies/cookies';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {WebView, WebViewNavigation} from 'react-native-webview';
+
+const INIT_URL = 'http://localhost:3000/script-cookie';
 
 export const AccessTokenFromScriptCookieScreen =
   'AccessTokenFromScriptCookieScreen';
 export default function AccessTokenFromScriptCookie() {
   const [webViewRef, setWebRef] = useState<WebView | null>();
-  // Using the reference
-  webViewRef?.injectJavaScript('console.log(window)');
+  const [currentURL, setCurrentURL] = useState(INIT_URL);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    CookieManager.clearAll(true).then(() => {
+      console.log(
+        '✅ CookieManager.clearAll() - Cleared cookies to prevent false positives',
+      );
+    });
+  }, []);
 
   // Listening to navigation events
   const handleWebViewNavigationStateChange = (
     newNavState: WebViewNavigation,
   ) => {
-    console.log('NAV STATE: ', newNavState);
+    if (newNavState.url !== currentURL) {
+      // Checking URL can take place here
+      setCurrentURL(newNavState.url);
+    }
+
+    // Checking token exists
+    CookieManager.getAll(true).then(cookies => {
+      if (cookies.auth) {
+        setToken(cookies.auth.value);
+        console.log(
+          '✅ CookieManager.getAll() - Got token: ',
+          JSON.stringify(cookies.auth || {}, null, 2),
+        );
+      }
+    });
   };
 
   return (
@@ -22,14 +54,31 @@ export default function AccessTokenFromScriptCookie() {
         <Text style={styles.pageTitle}>This WebView have a cookie</Text>
         <Text style={styles.pageQuestion}>Can you eat it?!</Text>
       </View>
+      {token && (
+        <View style={styles.tokenFoundContainer}>
+          <Text style={styles.tokenFoundText}>Token found Successfully!</Text>
+          <Text style={styles.tokenText}>{token}</Text>
+        </View>
+      )}
+      <View style={styles.toolBar}>
+        <TouchableOpacity onPress={webViewRef?.goBack}>
+          <Text>⏪ Back</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={webViewRef?.reload}>
+          <Text>🔄 Reload</Text>
+        </TouchableOpacity>
+      </View>
       <WebView
         ref={ref => setWebRef(ref)}
         // @ts-ignore - this is a mistyping in react-native-webview
         style={styles.webViewFrame}
-        source={{uri: 'http://localhost:3000'}}
+        source={{uri: INIT_URL}}
         onError={error => console.log('WebView ERROR: ', error)}
         onNavigationStateChange={handleWebViewNavigationStateChange}
         sharedCookiesEnabled={true}
+        startInLoadingState={true}
+        thirdPartyCookiesEnabled={true}
       />
     </SafeAreaView>
   );
@@ -43,7 +92,7 @@ const styles = StyleSheet.create({
   },
   header: {
     alignItems: 'center',
-    marginVertical: 30,
+    marginVertical: 20,
   },
   pageTitle: {
     fontSize: 20,
@@ -51,6 +100,25 @@ const styles = StyleSheet.create({
   pageQuestion: {
     fontSize: 26,
     fontWeight: 'bold',
+  },
+  tokenFoundContainer: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  tokenFoundText: {
+    fontSize: 24,
+    color: 'teal',
+  },
+  tokenText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  toolBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    paddingVertical: 10,
   },
   webViewFrame: {
     borderWidth: 3,
